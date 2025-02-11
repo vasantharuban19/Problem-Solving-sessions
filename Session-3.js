e4c44a35616ce6d953c01a9323aa297aa899972c
vasantharuban19 <vasanthruban1920@gmail.com>
2025-02-11 22:46:51 +0530

    updated

--- Session-3.js
+++ Session-3.js
@@ -2,68 +2,310 @@
 //* Session Focus: Basic string operations, string traversal, and manipulation techniques.
 
 // 1. Count vowels and consonants in a string.
+function countVowelsAndConsonants(str) {
+  const counter = { vowels: 0, consonants: 0, others: 0 };
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if ((char <= "Z" && char >= "A") || (char <= "z" && char >= "a")) {
+      if (
+        char == "A" ||
+        char == "E" ||
+        char == "I" ||
+        char == "O" ||
+        char == "U" ||
+        char == "a" ||
+        char == "e" ||
+        char == "i" ||
+        char == "o" ||
+        char == "u"
+      ) {
+        counter.vowels++;
+      } else {
+        counter.consonants++;
+      }
+    } else counter.others++;
+  }
+  return counter;
+}
+// console.log(countVowelsAndConsonants('vasanth123'));
 
+// 2. Reverse a string
+function reverse(str) {
+  let reversedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    reversedStr = str[i] + reversedStr;
+  }
+  return reversedStr;
+}
+// console.log(reverse("Hello World"));
 
-
-
-
-function upperCase(str){
-    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lower = "abcdefghijklmnopqrstuvwxyz"
-    const arr = [];
-    for (let ind = 0; ind < str.length; ind++){
-        let currind = lower.indexOf(str[ind])
-        if(currind != -1){
-            arr.push(upper[currind])
-        } else {
-            arr.push(str[ind])
-        }
+// 3. Convert a string to lowercase/upeercase
+function upperCase(str) {
+  let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lower = "abcdefghijklmnopqrstuvwxyz";
+  const arr = [];
+  for (let ind = 0; ind < str.length; ind++) {
+    let currind = lower.indexOf(str[ind]);
+    if (currind != -1) {
+      arr.push(upper[currind]);
+    } else {
+      arr.push(str[ind]);
     }
-    return arr.join("");
-        
-    }
+  }
+  return arr.join("");
+}
 console.log(upperCase("vasanth"));
 
-function toUpperCase(str){
-    const arr =[];
-    for (let ind = 0; ind < str.length; ind++){
-        if(str[ind] >= 'a' && str[ind] <= 'z'){
-            arr.push(String.fromCharCode(str.charCodeAt(ind) - 32))
-        } else {
-            arr.push(str[ind])
-        }
+function toUpperCase(str) {
+  const arr = [];
+  for (let ind = 0; ind < str.length; ind++) {
+    if (str[ind] >= "a" && str[ind] <= "z") {
+      arr.push(String.fromCharCode(str.charCodeAt(ind) - 32));
+    } else {
+      arr.push(str[ind]);
     }
-    return arr.join("")
+  }
+  return arr.join("");
 }
 console.log(toUpperCase("vasanth"));
 
-function lowerCase(str){
-    let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lower = "abcdefghijklmnopqrstuvwxyz"
-    const arr = [];
-    for (let ind = 0; ind < str.length; ind++){
-        let currind = upper.indexOf(str[ind])
-        if(currind != -1){
-            arr.push(lower[currind])
-        } else {
-            arr.push(str[ind])
-        }
+function lowerCase(str) {
+  let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lower = "abcdefghijklmnopqrstuvwxyz";
+  const arr = [];
+  for (let ind = 0; ind < str.length; ind++) {
+    let currind = upper.indexOf(str[ind]);
+    if (currind != -1) {
+      arr.push(lower[currind]);
+    } else {
+      arr.push(str[ind]);
     }
-    return arr.join("");
-        
-    }
+  }
+  return arr.join("");
+}
 console.log(lowerCase("VASANTH"));
 
-function toLowerCase(str){
-    const arr =[];
-    for (let ind = 0; ind < str.length; ind++){
-        if(str[ind] >= 'A' && str[ind] <= 'Z'){
-            arr.push(String.fromCharCode(str.charCodeAt(ind) + 32))
-        } else {
-            arr.push(str[ind])
-        }
+function toLowerCase(str) {
+  const arr = [];
+  for (let ind = 0; ind < str.length; ind++) {
+    if (str[ind] >= "A" && str[ind] <= "Z") {
+      arr.push(String.fromCharCode(str.charCodeAt(ind) + 32));
+    } else {
+      arr.push(str[ind]);
     }
-    return arr.join("")
+  }
+  return arr.join("");
 }
-console.log(toLowerCase("VASANTH"));
+console.log(toLowerCase("VaSaNtH"));
 
+// 4. Find the longest word in a sentence.
+function longestWord(str) {
+  let start = 0;
+  let maxLen = 0;
+  let maxStart = -1;
+  let maxEnd = -1;
+  let ind = 1;
+  for (ind; ind <= str.length; ind++) {
+    if (ind == str.length || str[ind] == " ") {
+      let len = ind - start;
+      if (len > maxLen) {
+        maxLen = len;
+        maxStart = start;
+        maxEnd = ind;
+      }
+      start = ind + 1;
+    }
+  }
+  return str.substring(maxStart, maxEnd);
+}
+console.log(longestWord("Hello World! This is a test sentence."));
+
+// 5. Check if a string Palindrome.
+// 1st method using while
+function isPalindromee(str) {
+  let start = 0;
+  let end = str.length - 1;
+  while (start < end) {
+    if (str[start] != str[end]) return false;
+    start++;
+    end--;
+  }
+  return true;
+}
+console.log(isPalindromee("A man, a plan, a canal: Panama"));
+// 2nd method for loop
+function palindrome(str) {
+  let reversed = "";
+  for (let i = 0; i < str.length; i++) {
+    reversed = str[i] + reversed;
+  }
+  return reversed === str;
+}
+console.log(palindrome("A man, a plan, a canal: Panama"));
+// 3rd method
+function isPalindrome(str) {
+  function isAlpanumeric(char) {
+    return (
+      (char >= "a" && char <= "z") ||
+      (char >= "A" && char <= "Z") ||
+      (char >= "0" && char <= "9")
+    );
+  }
+  let start = 0;
+  let end = str.length - 1;
+  while (start < end) {
+    while (start < end && !isAlpanumeric(str[start])) start++;
+    while (start < end && !isAlpanumeric(str[end])) end--;
+
+    if (str[start].toLowerCase() !== str[end].toLowerCase()) return false;
+    start++;
+    end--;
+  }
+  return true;
+}
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+
+// 6. Remove duplicates from a string
+function removeDuplicates(str) {
+  let removedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (removedStr.indexOf(char) == -1) {
+      removedStr += char;
+    }
+  }
+  return removedStr;
+}
+console.log(removeDuplicates("banana"));
+
+// 7. find all substrings of a given string
+function findSubStrings(str) {
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    let temp = "";
+    for (let ind = i; ind < str.length; ind++) {
+      temp += str[ind];
+      arr.push(temp);
+    }
+  }
+  return arr;
+}
+console.log(findSubStrings("abc"));
+
+// Practice Question
+// 1. Concatenate two strings.
+function concatenateString(str1, str2) {
+  return str1 + str2;
+}
+console.log(concatenateString("vasanth", "ruban"));
+
+// 2. Find the frequency of each character in a string.
+function charFrequency(str) {
+  let freqMap = {};
+  for (let char of str) {
+    freqMap[char] = (freqMap[char] || 0) + 1;
+  }
+  return freqMap;
+}
+console.log(charFrequency("vasanth"));
+
+// 3. Replace spaces in a string with %20.
+function replaceSpace(str) {
+  return str.split(" ").join("%20");
+}
+console.log(replaceSpace("h e l l o"));
+
+// 4. Check if a string is an anagram of another
+// 1st method sorting
+function isAnagram(str1, str2) {
+  if (str1.length != str2.length) return false;
+
+  return str1.split("").sort().join("") == str2.split("").sort().join("");
+}
+console.log(isAnagram("vasanth", "htnasav"));
+// 2nd method using frequency
+function anagram(str1, str2) {
+  if (str1.length != str2.length) return false;
+  let freqMap = {};
+  for (let char of str1) freqMap[char] = (freqMap[char] || 0) + 1;
+  for (let char of str2) {
+    if (!freqMap[char]) return false;
+    freqMap[char]--;
+  }
+  return true;
+}
+console.log(anagram("Hello", "elloH"));
+
+// 5. Count the number of words in a sentence.
+function countWords(str) {
+  let count = 0;
+  let inWord = false;
+  for (let char of str) {
+    if (char !== " ") {
+      if (!inWord) count++;
+      inWord = true;
+    } else inWord = false;
+  }
+  return count;
+}
+console.log(countWords("Hii this is vasanth"));
+
+// 6. Find the first non-repeating character in a string
+function firstNonRepeatingChar(str) {
+  let freq = {};
+  for (let char of str) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  for (let char of str) {
+    if (freq[char] == 1) {
+      return char;
+    }
+  }
+  return null;
+}
+console.log(firstNonRepeatingChar("character"));
+
+// 7. Remove all vowels from a string.
+function removeVowels(str) {
+  let vowels = "aeiouAEIOU";
+  let result = "";
+  for (let char of str) {
+    if (!vowels.includes(char)) {
+      result += char;
+    }
+  }
+  return result;
+}
+console.log(removeVowels("vasanthaeiou"));
+
+// 8. Find the shortest word in a sentence.
+function findShortestWord(str) {
+  let short = "";
+  let currWord = "";
+  let minLength = Infinity;
+  for (let i = 0; i <= str.length; i++) {
+    if (i == str.length || str[i] == " ") {
+      if (currWord.length > 0 && currWord.length < minLength) {
+        short = currWord;
+        minLength = currWord.length;
+      }
+      currWord = "";
+    } else {
+      currWord += str[i];
+    }
+  }
+  return short || null;
+}
+console.log(findShortestWord("find the shortest word"));
+
+// 9. Count occurrences of a substring within a string.
+function countOccurrence(str, subStr) {
+  let count = 0;
+  let index = 0;
+  while ((index = str.indexOf(subStr, index)) !== -1) {
+    count++;
+    index++;
+  }
+  return count;
+}
+console.log(countOccurrence("apple pie apple pie", "apple"));
--- Session-4.js
+++ Session-4.js
@@ -1,8 +1,14 @@
-function maxminElement(arr =[]) {
+// Day-4: Arrays & Array Manipulations
+// Session Focus: Array creation, traversal, and manipulation techniques.
+
+// 1. Find the maximum and minimum elements in an array.
+function maxminElement(arr = []) {
+  console.log(arr);
+
   let max = arr[0];
   let min = arr[0];
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i];
     }
@@ -10,37 +16,288 @@ function maxminElement(arr =[]) {
       min = arr[i];
     }
   }
-  return {max,min};
+  return [max, min];
 }
+// console.log(maxminElement([10, 20, 30, 40, 50]));
+console.log(
+  maxminElement(
+    Array.from({ length: 5 }, () => Math.floor(Math.random() * 100))
+  )
+);
 
-console.log(maxminElement([10, 20, 30, 40, 50]));
-
-function sumElement(arr){
-    let sum = 0;
-
-    for(let i = 0; i < arr.length; i++){
-        sum = sum + arr[i];
+// 2. Find the second - largest element in an array.
+function secLargest(arr = []) {
+  console.log(arr);
+  if (arr.length < 2) return null;
+  let max = arr[0];
+  let secMax = -Infinity;
+  for (let i = 1; i < arr.length; i++) {
+    if (max < arr[i]) {
+      secMax = max;
+      max = arr[i];
+    } else if (arr[i] < max && arr[i] > secMax) {
+      secMax = arr[i];
     }
-    return sum;
+  }
+  return { max, secMax };
 }
+// console.log(secLargest([10,20,5,15,20]));
+console.log(
+  secLargest(Array.from({ length: 10 }, () => Math.floor(Math.random() * 100)))
+);
 
-console.log(sumElement([1, 2, 3, 4, 5]));
-
-function findPeakElement(arr) {
-    let left = 0;
-    let right = arr.length - 1;
-
-    while (left < right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (arr[mid] < arr[mid + 1]) {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
+// 3. Sort an array of integers.
+// Selection Sort
+function sort(arr = []) {
+  // console.log(arr);
+  for (let i = 0; i < arr.length; i++) {
+    let minInd = i;
+    for (let ind = i + 1; ind < arr.length; ind++) {
+      if (arr[minInd] > arr[ind]) minInd = ind;
     }
-
-    return left; 
+    if (i !== minInd) {
+      [arr[i], arr[minInd]] = [arr[minInd], arr[i]];
+    }
+  }
+  return arr;
 }
+console.log(
+  sort(Array.from({ length: 10 }, () => Math.floor(Math.random() * 100)))
+);
 
-console.log(findPeakElement([1, 3, 5, 7, 6, 4, 2]));
+// 4.Rotate an array by one position.
+function rotateOne(arr = []) {
+  console.log(arr);
+  let n = arr.length;
+  let temp = arr[n - 1];
+  for (let i = n - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = temp;
+  return arr;
+}
+// console.log(rotateOne(sort([1, 2, 3, 4, 5])));
+console.log(
+  rotateOne(
+    sort(Array.from({ length: 5 }, () => Math.floor(Math.random() * 10)))
+  )
+);
+
+// 5. Remove duplicates from a sorted array.
+function removeDuplicates(arr = []) {
+  let unique = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] != arr[i]) {
+      unique.push(arr[i]);
+    }
+  }
+  // console.log(unique.length);
+  return unique;
+}
+console.log(removeDuplicates(sort([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])));
+
+// 6. Calculate the sum of elements in an array
+function sum(arr = []) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+console.log(sum([1, 2, 3, 4, 5]));
+
+function rightSidedSum(arr = []) {
+  let total = 0;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    total += arr[i];
+    arr[i] = total - arr[i];
+  }
+  return arr;
+}
+console.log(rightSidedSum([1, 2, 3, 4]));
+
+// 7. Find the number of occurrences of an element in an array.
+function findOcc(arr = [], elem = 2) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == elem) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(findOcc([1, 2, 3, 2, 4, 2, 5, 2]));
+
+// Session Practice question
+
+// 1. Merge two sorted arrays
+function mergeSortedArray(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let merged = [];
+  console.log(arr1, arr2);
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
+  }
+  return merged;
+}
+console.log(
+  mergeSortedArray(
+    sort(Array.from({ length: 5 }, () => Math.floor(Math.random() * 10))),
+    sort(Array.from({ length: 5 }, () => Math.floor(Math.random() * 10)))
+  )
+);
+
+// 2. Reverse the elements in an array.
+function reverseArray(arr = []) {
+  let left = 0;
+  let right = arr.length - 1;
+  console.log(arr);
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+  return arr;
+}
+console.log(
+  reverseArray(
+    sort(Array.from({ length: 5 }, () => Math.floor(Math.random() * 10)))
+  )
+);
+
+// 3. Rotate an array to the left by K positions.
+function rotateLeft(arr = [], k) {
+  let n = arr.length;
+  let left = 0;
+  let right = k - 1;
+  k = k % n;
+  if (n == 0) return arr;
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+  left = k;
+  right = n - 1;
+
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+  left = 0;
+  right = n - 1;
+
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+  return arr;
+}
+console.log(rotateLeft([1, 2, 3, 4, 5], 2));
+
+// 4. Search for an element in a sorted array.
+function binarySearch(arr = [], target = 23) {
+  let left = 0;
+  let right = arr.length - 1;
+  console.log(arr);
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] == target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+console.log(
+  binarySearch(
+    sort(Array.from({ length: 100 }, () => Math.floor(Math.random() * 100)))
+  )
+);
+
+// 5. Find the cumulative sum of an array.
+function cumulativeSum(arr = []) {
+  let result = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    result.push(sum);
+  }
+  return result;
+}
+console.log(cumulativeSum([1, 2, 3, 4, 5]));
+
+// 6. Calculate the product of all elements in an array.
+function calculateProduct(arr = []) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
+}
+console.log(calculateProduct([1, 2, 3, 4, 5]));
+
+// 7. Check if an array is a palindrome.
+function isPalindrome(arr = []) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    if (arr[left] !== arr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+console.log(isPalindrome([1, 2, 3, 2, 1]));
+
+// 8. Find the intersection of two arrays. *important
+function intersection(arr1, arr2) {
+  const map = new Map();
+  const result = [];
+
+  for (let num of arr1) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  for (let num of arr2) {
+    if (map.get(num) > 0) {
+      result.push(num);
+      map.set(num, map.get(num) - 1);
+    }
+  }
+  return result;
+}
+const arr1 = Array.from({ length: 5 }, () =>
+  Math.floor(Math.random() * 10)
+).sort((a, b) => a - b);
+console.log(arr1);
+
+const arr2 = Array.from({ length: 5 }, () =>
+  Math.floor(Math.random() * 10)
+).sort((a, b) => a - b);
+console.log(arr2);
+
+console.log(intersection(arr1,arr2));
+      
